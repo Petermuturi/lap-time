42c269a664a90182086ee50b584e225362059bd5
Petermuturi <pmuturi65@gmail.com>
2017-01-14 20:02:05 +0300

    modified component layouts

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
 
 import Watch from '../watch/Watch'
-import Trigger from '../triggers/Trigger'
+import './main.css'
 
 export default class Main extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0,
+      minute: "00",
+      second: "00",
+      minSec: "0",
+    }
+  }
+
   render(){
     return(
-      <Container style={this.background()}>
-        <Watch/>
-        <div style={this.triangle()}>
+      <div style={this.background()}>
+        <Watch states={this.state} />
+        <div style={this.triangle()}/>
+        <div className="trigger">
+          <div className="stlp">
+            <a className="start">start</a>
+            <a className="lap">lap</a>
+            <a className="reset">reset</a>
+          </div>
+          <br/>
+          <hr/>
         </div>
-        <Trigger/>
-      </Container>
+      </div>
     )
   }
+
   background(){
     var imageUrl = (Math.random() * 4).toFixed();
-    if(imageUrl === "0"){
+    if(imageUrl === 0){
       imageUrl = parseInt(imageUrl, 10) + 1
+    }
+    if(imageUrl >= 5){
+      imageUrl = parseInt(imageUrl, 10) - 1
     }
     const img = imageUrl.toString();
     console.log(img);
--- src/watch/Watch.js
+++ src/watch/Watch.js
@@ -3,25 +3,16 @@ import React, { Component } from 'react'
 import './watch.css'
 
 export default class Watch extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      count: 0,
-      minute: "00",
-      second: "00",
-      minSec: "0",
-    }
-  }
-
   render(){
+    const { states } = this.props
     return(
       <div>
         <div className="timer">
-          <span className="min">{this.state.minute}</span>
+          <span className="min">{states.minute}</span>
           :
-          <span className="sec">{this.state.second}</span>
+          <span className="sec">{states.second}</span>
           :
-          <span className="minsec">{this.state.minSec}</span>
+          <span className="minsec">{states.minSec}</span>
         </div>
       <p className="lap-time">Lap time!</p>
       </div>
