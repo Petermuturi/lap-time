331f461dbd3d8f2cea68fdcbfdfb357773eb570e
Petermuturi <pmuturi65@gmail.com>
2017-01-14 15:53:22 +0300

    added triangle positioning

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Container } from 'semantic-ui-react'
+
 import Watch from '../watch/Watch'
+// import Trigger from '../triggers/Trigger'
 
 export default class Main extends Component{
   render(){
@@ -8,7 +10,7 @@ export default class Main extends Component{
       <Container style={this.background()}>
         <Watch/>
         <div style={this.triangle()}>
-
+          {/* <Trigger/> */}
         </div>
       </Container>
     )
@@ -19,7 +21,6 @@ export default class Main extends Component{
       imageUrl = parseInt(imageUrl, 10) + 1
     }
     const img = imageUrl.toString();
-    console.log("url(img/"+img+".jpg)");
     return{
       margin:"-8px -8px -10px -8px",
       background:"url(img/"+img+".jpg)",
@@ -31,8 +32,10 @@ export default class Main extends Component{
   }
   triangle(){
     return{
+      position:"relative",
+      color:"white",
       paddingLeft:"46%",
-      marginTop:"-20px",
+      marginTop:"-18px",
       width:"0",
       height:"0",
       borderBottom: "100vh solid white",
---
+++ src/triggers/Trigger.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Trigger extends Component{
+  render(){
+    return(
+      <h1>Simple StopWatch</h1>
+    )
+  }
+}
